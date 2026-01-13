33e495bf8c282b6830c2da5645c9c16ad6b82ab2
zena876 <nzena605@gmail.com>
2026-01-13 15:26:56 +0300

    Update __init__.py

--- recipebook/__init__.py
+++ recipebook/__init__.py
@@ -1,13 +1,10 @@
 
 #RecipeBook - Пакет для управления кулинарными рецептами.
 
-version = "1.0.0"
-author = "Наторова Евгения Михайловна"
-email = "nzena605@gmail.com"
-
 from .ingredient import Ingredient
 from .recipe import Recipe
 from .cookbook import Cookbook
 
 
 __all__ = ['Ingredient', 'Recipe', 'Cookbook']
+
